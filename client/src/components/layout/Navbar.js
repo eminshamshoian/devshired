2b8170374d28e512ee53d63653a8464da68e1ff7
Emin Shamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-02-28 15:10:26 -0800

    created profile component and started on dashboard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,11 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to='/register'>
+          <i className='fas fa-user'></i> <span>Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href='#!'>
           <i className='fas fa-sign-out-alt'></i>{' '}
           <span className='hide-sm'>Logout</span>

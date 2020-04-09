bed180aefac35a25f5dd3f0e22c8f438428c28b3
eminshamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-04-08 23:30:50 -0700

    Edit profile function created

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 import PrivateRoute from './components/routing/PrivateRoute';
 import './App.css';
 // Redux
@@ -17,38 +18,51 @@ import setAuthToken from './utils/setAuthToken';
 
 // Set header with token, if there is one
 if (localStorage.token) {
-  setAuthToken(localStorage.token);
+    setAuthToken(localStorage.token);
 }
 
 const App = () => {
-  //Brackets make it only run once - like componentDidMount()
-  useEffect(() => {
-    store.dispatch(loadUser());
-  }, []);
+    //Brackets make it only run once - like componentDidMount()
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, []);
 
-  return (
-    <Provider store={store}>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <Route exact path='/' component={Landing} />
-          <section className='container'>
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute
-                exact
-                path='/create-profile'
-                component={CreateProfile}
-              />
-            </Switch>
-          </section>
-        </Fragment>
-      </Router>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <Router>
+                <Fragment>
+                    <Navbar />
+                    <Route exact path='/' component={Landing} />
+                    <section className='container'>
+                        <Alert />
+                        <Switch>
+                            <Route
+                                exact
+                                path='/register'
+                                component={Register}
+                            />
+                            <Route exact path='/login' component={Login} />
+                            <PrivateRoute
+                                exact
+                                path='/dashboard'
+                                component={Dashboard}
+                            />
+                            <PrivateRoute
+                                exact
+                                path='/create-profile'
+                                component={CreateProfile}
+                            />
+                            <PrivateRoute
+                                exact
+                                path='/edit-profile'
+                                component={EditProfile}
+                            />
+                        </Switch>
+                    </section>
+                </Fragment>
+            </Router>
+        </Provider>
+    );
 };
 
 export default App;

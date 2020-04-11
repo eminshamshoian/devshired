661b67c9add7b4ce1b4b27899065f3032ea780c8
eminshamshoian <eminshamshoian@Emins-MacBook-Pro.local>
2020-04-11 00:14:46 -0700

    Added user deletion functionality

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 import { setAlert } from './alert';
 
-import { GET_PROFILE, UPDATE_PROFILE, PROFILE_ERROR } from './types';
+import {
+    GET_PROFILE,
+    UPDATE_PROFILE,
+    PROFILE_ERROR,
+    CLEAR_PROFILE,
+    ACCOUNT_DELETED
+} from './types';
 
 // Get the current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -138,5 +144,77 @@ export const addEducation = (formData, history) => async dispatch => {
                 status: err.response.status
             }
         });
+    }
+};
+
+// Delete Experience
+export const deleteExperience = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/experience/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Experience Deleted', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        });
+    }
+};
+
+// Delete Education
+export const deleteEducation = id => async dispatch => {
+    try {
+        const res = await axios.delete(`/api/profile/education/${id}`);
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert('Education Deleted', 'success'));
+    } catch (err) {
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: err.response.statusText,
+                status: err.response.status
+            }
+        });
+    }
+};
+
+// Delete Account and Profile
+// Delete Education
+export const deleteAccount = () => async dispatch => {
+    if (window.confirm('Are you sure? This can NOT be reversed!')) {
+        try {
+            const res = await axios.delete(`/api/profile`);
+
+            dispatch({
+                type: CLEAR_PROFILE
+            });
+
+            dispatch({
+                type: ACCOUNT_DELETED
+            });
+
+            dispatch(setAlert('Your account has been permanantly deleted!'));
+        } catch (err) {
+            dispatch({
+                type: PROFILE_ERROR,
+                payload: {
+                    msg: err.response.statusText,
+                    status: err.response.status
+                }
+            });
+        }
     }
 };
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User');
 const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
 //@route    GET api/profile/me
 //@desc     Get current users profile
@@ -160,7 +161,8 @@ router.get('/user/:user_id', async (req, res) => {
 //@access   Private
 router.delete('/', auth, async (req, res) => {
     try {
-        //@TODO - remove users posts
+        // Remove User Post
+        await Post.deleteMany({ user: req.user.id });
 
         //Remove profile
         await Profile.findOneAndRemove({ user: req.user.id });
